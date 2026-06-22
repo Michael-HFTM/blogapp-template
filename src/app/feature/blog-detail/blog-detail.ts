5fc9dc1c81fccdc2503e01cbe6f4fba3c21a402f
Michael Gasser <michael.gasser@hftm.ch>
2026-06-22 19:40:11 +0200

    feat: implement blog detail view, add routerLink for BlogCard, improve navigation

--- src/app/feature/blog-detail/blog-detail.ts
+++ src/app/feature/blog-detail/blog-detail.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { BlogService } from '../../shared/blog/blog';
 
 @Component({
   selector: 'app-blog-detail',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './blog-detail.html',
   styleUrl: './blog-detail.scss',
 })
-export class BlogDetail {}
+export class BlogDetail {
+  id = input.required<string>();
+  private readonly blogService = inject(BlogService);
+
+  protected readonly blog = computed(() => {
+    const blogId = Number(this.id());
+    return this.blogService.getById(blogId);
+  });
+}
--- src/app/shared/blog-card/blog-card.ts
+++ src/app/shared/blog-card/blog-card.ts
@@ -3,10 +3,11 @@ import { Blog } from '../../models/blog.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-card',
-  imports: [MatCardModule, MatButtonModule, MatIcon],
+  imports: [MatCardModule, MatButtonModule, MatIcon, RouterLink],
   templateUrl: './blog-card.html',
   styleUrl: './blog-card.scss',
 })
