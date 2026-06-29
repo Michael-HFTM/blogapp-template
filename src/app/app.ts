d4ccf947279dec4355997604ffb971c45a03fcf0
Michael Gasser <michael.gasser@hftm.ch>
2026-06-29 14:17:53 +0200

    feat: add custom theme palettes, light/dark mode support, and theme toggle functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly document = inject(DOCUMENT);
+
   protected readonly title = 'HFTM Web Applications (IN353)';
+  protected readonly isDark = signal(false);
+
+  toggleTheme() {
+    this.isDark.update((dark) => !dark);
+    this.document.documentElement.classList.toggle('dark-theme', this.isDark());
+  }
 }
