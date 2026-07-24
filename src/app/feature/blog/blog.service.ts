c903ca5c8e95c9f48402314af523b3bc832a71f1
Michael Gasser <michael.gasser@hftm.ch>
2026-07-24 11:56:01 +0200

    refactor: align BlogService/BlogOverview structure with upstream
    Merge BlogList into BlogOverview and make BlogService stateless, matching the component/service split used upstream ahead of the HTTP-based BlogService.

--- src/app/feature/blog/blog.service.ts
+++ src/app/feature/blog/blog.service.ts
@@ -1,30 +1,14 @@
-import { Service, signal } from '@angular/core';
+import { Service } from '@angular/core';
 import blogData from '../../data/blogs.json';
 import { Blog } from './blog.model';
 
 @Service()
 export class BlogService {
-  private readonly blogs = signal<Blog[]>(blogData as Blog[]);
-
-  public getAll() {
-    return this.blogs.asReadonly();
+  public getBlogs(): Blog[] {
+    return blogData as Blog[];
   }
 
-  public getById(id: number) {
-    return this.blogs().find((blog) => blog.id === id);
-  }
-
-  public handleLike(blogId: number): void {
-    this.blogs.update((blogs) =>
-      blogs.map((blog) => (blog.id === blogId ? this.updateLikesOnBlog(blog) : blog)),
-    );
-  }
-
-  private updateLikesOnBlog(blog: Blog): Blog {
-    return {
-      ...blog,
-      likedByMe: !blog.likedByMe,
-      likes: blog.likedByMe ? blog.likes - 1 : blog.likes + 1,
-    };
+  public getById(id: number): Blog | undefined {
+    return (blogData as Blog[]).find((blog) => blog.id === id);
   }
 }
